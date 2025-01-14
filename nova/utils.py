d2a4da115102114d8c37580f00767d5796cfe060
Ryan Partridge <rpartridge101@yahoo.co.uk>
2025-01-14 23:21:21 +0000

    build(deps): Added missing deps and dev deps for vscode.

--- nova/utils.py
+++ nova/utils.py
@@ -2,7 +2,11 @@ import torch
 
 
 def set_device(device: str) -> torch.device:
-    """Configures the `torch.device` based on the device `string` input."""
+    """
+    Configures the `torch.device` based on the device `string` input.
+
+    If `device=auto`, automatically loads onto `cuda` if available. Else, `cpu`.
+    """
     if device == "auto":
         return torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
